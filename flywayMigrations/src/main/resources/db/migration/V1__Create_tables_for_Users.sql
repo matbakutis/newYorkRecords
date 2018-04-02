37328e6ee5372f8decdf674cc65f503e02452849
matbakutis <31048002+matbakutis@users.noreply.github.com>
2018-04-02 10:46:37 -0700

    added postgres migrations

--- flywayMigrations/src/main/resources/db/migration/V1__Create_tables_for_Users.sql
+++ flywayMigrations/src/main/resources/db/migration/V1__Create_tables_for_Users.sql
@@ -2,5 +2,5 @@ CREATE TABLE users (
     ID serial,
     username varchar(100) not null,
     firstName varchar(100) not null,
-    lastName varchar(100) not null,
+    lastName varchar(100) not null
 );
