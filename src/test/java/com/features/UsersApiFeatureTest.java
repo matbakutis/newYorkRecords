daf833d41d5d3802ac2eb556e0a095b353df20f0
matbakutis <31048002+matbakutis@users.noreply.github.com>
2018-04-02 09:40:12 -0700

    created failing users api feature test for crud functionality

--- src/test/java/com/features/UsersApiFeatureTest.java
+++ src/test/java/com/features/UsersApiFeatureTest.java
@@ -38,82 +38,82 @@ public class UsersApiFeatureTest {
     @Test
     public void shouldAllowFullCrudForAUser() throws Exception {
 
-//        User firstUser = new User(
-//                "someone",
-//                "Ima",
-//                "Person"
-//        );
-//
-//        User secondUser = new User(
-//                "someone_else",
-//                "Someone",
-//                "Else"
-//        );
-//
-//        Stream.of(firstUser, secondUser)
-//                .forEach(user -> {
-//                    userRepository.save(user);
-//                });
-//
-//        // Test get all Users
-//        when()
-//                .get("http://localhost:8090/users/")
-//                .then()
-//                .statusCode(is(200))
-//                .and().body(containsString("someone"))
-//                .and().body(containsString("Else"));
-//
-//
-//        // Test creating a User
-//        User userNotYetInDb = new User(
-//                "new_user",
-//                "Not",
-//                "Yet Created"
-//        );
-//
-//        given()
-//                .contentType(JSON)
-//                .and().body(userNotYetInDb)
-//                .when()
-//                .post("http://localhost:8080/users")
-//                .then()
-//                .statusCode(is(200))
-//                .and().body(containsString("new_user"));
-//
-//        // Test get all Users
-//        when()
-//                .get("http://localhost:8080/users/")
-//                .then()
-//                .statusCode(is(200))
-//                .and().body(containsString("someone"))
-//                .and().body(containsString("Else"))
-//                .and().body(containsString("Yet Created"));
-//
-//        // Test finding one user by ID
-//        when()
-//                .get("http://localhost:8080/users/" + secondUser.getId())
-//                .then()
-//                .statusCode(is(200))
-//                .and().body(containsString("Someone"))
-//                .and().body(containsString("Else"));
-//
-//        // Test updating a user
-//        secondUser.setFirstName("changed_name");
-//
-//        given()
-//                .contentType(JSON)
-//                .and().body(secondUser)
-//                .when()
-//                .patch("http://localhost:8080/users/" + secondUser.getId())
-//                .then()
-//                .statusCode(is(200))
-//                .and().body(containsString("changed_name"));
-//
-//        // Test deleting a user
-//        when()
-//                .delete("http://localhost:8080/users/" + secondUser.getId())
-//                .then()
-//                .statusCode(is(200));
+        User firstUser = new User(
+                "someone",
+                "Ima",
+                "Person"
+        );
+
+        User secondUser = new User(
+                "someone_else",
+                "Someone",
+                "Else"
+        );
+
+        Stream.of(firstUser, secondUser)
+                .forEach(user -> {
+                    userRepository.save(user);
+                });
+
+        // Test get all Users
+        when()
+                .get("http://localhost:8090/users/")
+                .then()
+                .statusCode(is(200))
+                .and().body(containsString("someone"))
+                .and().body(containsString("Else"));
+
+
+        // Test creating a User
+        User userNotYetInDb = new User(
+                "new_user",
+                "Not",
+                "Yet Created"
+        );
+
+        given()
+                .contentType(JSON)
+                .and().body(userNotYetInDb)
+                .when()
+                .post("http://localhost:8090/users")
+                .then()
+                .statusCode(is(200))
+                .and().body(containsString("new_user"));
+
+        // Test get all Users
+        when()
+                .get("http://localhost:8090/users/")
+                .then()
+                .statusCode(is(200))
+                .and().body(containsString("someone"))
+                .and().body(containsString("Else"))
+                .and().body(containsString("Yet Created"));
+
+        // Test finding one user by ID
+        when()
+                .get("http://localhost:8090/users/" + secondUser.getId())
+                .then()
+                .statusCode(is(200))
+                .and().body(containsString("Someone"))
+                .and().body(containsString("Else"));
+
+        // Test updating a user
+        secondUser.setFirstName("changed_name");
+
+        given()
+                .contentType(JSON)
+                .and().body(secondUser)
+                .when()
+                .patch("http://localhost:8090/users/" + secondUser.getId())
+                .then()
+                .statusCode(is(200))
+                .and().body(containsString("changed_name"));
+
+        // Test deleting a user
+        when()
+                .delete("http://localhost:8090/users/" + secondUser.getId())
+                .then()
+                .statusCode(is(200));
     }
 
 }
