f21be92fc10fcf368bb27ae60c8a0f0617d8c39f
matbakutis <31048002+matbakutis@users.noreply.github.com>
2018-04-02 11:08:10 -0700

    get all users test passes

--- src/main/java/com/example/backendusers/controllers/UserController.java
+++ src/main/java/com/example/backendusers/controllers/UserController.java
@@ -1,14 +1,20 @@
 package com.example.backendusers.controllers;
 
+import com.example.backendusers.models.User;
+import com.example.backendusers.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/users")
-    public String findAllUsers() {
-        return "It's working!!!";
+    public Iterable<User> findAllUsers() {
+        return userRepository.findAll();
     }
 
 }
--- src/test/java/com/controllers/UserControllerTest.java
+++ src/test/java/com/controllers/UserControllerTest.java
@@ -2,22 +2,61 @@ package com.controllers;
 
 
 import com.example.backendusers.controllers.UserController;
+import com.example.backendusers.models.User;
+import com.example.backendusers.repositories.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
 
+    @MockBean
+    private UserRepository mockUserRepository;
+
     @Autowired
     private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        User firstUser = new User(
+                "someone",
+                "Ima",
+                "Person"
+        );
+
+        User secondUser = new User(
+                "someone_else",
+                "Someone",
+                "Else"
+        );
+
+        Iterable<User> mockUsers =
+                Stream.of(firstUser, secondUser).collect(Collectors.toList());
+
+        given(mockUserRepository.findAll()).willReturn(mockUsers);
+    }
 
     @Test
     public void findAllUsers_success_returnsStatusOK() throws Exception {
@@ -25,6 +64,14 @@ public class UserControllerTest {
         this.mockMvc
                 .perform(get("/users"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void findAllUsers_success_returnAllUsersAsJSON() throws Exception {
+
+        this.mockMvc
+                .perform(get("/users"))
+                .andExpect(jsonPath("$", hasSize(2)));
     }
 
 }
