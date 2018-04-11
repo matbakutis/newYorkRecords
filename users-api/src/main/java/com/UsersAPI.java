1526a5b14a87cdf18ac9ce1e43df650fbf345090
matbakutis <31048002+matbakutis@users.noreply.github.com>
2018-04-11 09:40:29 -0700

    created microservices gateway/server

--- users-api/src/main/java/com/UsersAPI.java
+++ users-api/src/main/java/com/UsersAPI.java
@@ -2,6 +2,7 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,6 +14,7 @@ import java.util.Collections;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UsersAPI {
 
 	public static void main(String[] args) {
