adb4575b6ca0bf16de859a50efaa33931d5ed6e6
matbakutis <31048002+matbakutis@users.noreply.github.com>
2018-04-13 09:49:58 -0700

    created skeleton files for forum and post

---
+++ user-interface/src/components/Forum.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Forum extends Component {
+    constructor(){
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Forum;
