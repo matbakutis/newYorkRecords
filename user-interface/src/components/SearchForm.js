e25f40df24662b5e88e7a75dace72d2184a1bd92
matbakutis <31048002+matbakutis@users.noreply.github.com>
2018-04-13 09:37:41 -0700

    created search form and form for results

--- user-interface/src/components/SearchForm.js
+++ user-interface/src/components/SearchForm.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react';
+import SearchResult from './SearchResult'
 
 class SearchForm extends Component {
     constructor(){
         this.state = {
-            results: []
+            results: [],
+            search: ''
         }
     }
 
+    getResults = async (search) => {
+        if (search) {
+            const resultsResponse = await axios.get(`https://data.cityofnewyork.us/resource/buex-bi6w.json?type_of_notice_description=${search}`)
+            this.setState({
+                results: resultsResponse.data
+            })
+        }else {
+            const resultsResponse = await axios.get('https://data.cityofnewyork.us/resource/buex-bi6w.json')
+            this.setState({
+                results: resultsResponse.data
+            })
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.get
+    }
+
+    handleChange = (event) => {
+        const attributeToChange = event.target.name
+        const newValue = event.target.value;
+        this.setState({ attributeToChange: newValue })
+    }
+
     render() {
+
+        const searchResults = this.state.results.map((result, i)=>{
+            return <SearchResult result={result} key={i} />
+        })
+
         return (
             <div>
-                
+                <div>
+                    <h3>Search by Type of Notice</h3>
+                    <input
+                            id="search-bar"
+                            type="text"
+                            name="search"
+                            onChange={this.handleChange} required/>
+                </div>
+                <div>
+                    <h3>Results</h3>
+                    {searchResults}
+                </div>
             </div>
         );
     }
--- user-interface/src/components/SearchResult.js
+++ user-interface/src/components/SearchResult.js
@@ -3,7 +3,12 @@ import React from 'react';
 const SearchResult = (props) => {
     return (
         <div>
-            
+            { this.props.result.agency_name ? <h4>Agency: {props.result.agency_name}</h4> : null }
+            { this.props.result.category_description ? <h4>Category: {props.result.category_description}</h4> : null }
+            { this.props.result.vendor_name ? <h4>Vendor: {props.result.vendor_name}</h4> : null }
+            { this.props.result.request_id ? <h4>Request Id: {props.result.request_id}</h4> : null }
+            { this.props.result.short_title ? <h4>Short Title: {props.result.short_title}</h4> : null }
+            { this.props.result.printout_1 ? <p>Description: {props.result.printout_1}</p> : null }
         </div>
     );
 };
